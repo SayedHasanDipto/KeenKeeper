e43aee2f836a13181c25862e7c92c11c74aa2593
Sayed Hasan Dipto <sayedhasandipto@gmail.com>
2026-04-15 04:55:19 +0600

    finisshing details layer

--- src/component/pages/FriendDetails.jsx
+++ src/component/pages/FriendDetails.jsx
@@ -3,6 +3,9 @@ import { useLoaderData, useParams } from 'react-router';
 import { HiBellSnooze } from "react-icons/hi2";
 import { FaArchive } from "react-icons/fa";
 import { ImBin } from "react-icons/im";
+import { TbPhoneCall } from "react-icons/tb";
+import { MdOutlineSms } from "react-icons/md";
+import { FiVideo } from "react-icons/fi";
 
 const FriendDetails = () => {
     const { id } = useParams();
@@ -76,21 +79,47 @@ const FriendDetails = () => {
 
                 </div>
             </div>
-            <div className='w-full grid gap-4 grid-cols-3'>
-                <div className='w-full h-ful bg-base-100 shadow rounded-lg text-center flex justify-center items-center flex-col'>
+            <div className='w-full grid gap-4 grid-cols-3 grid-rows-3'>
+                <div className='w-full h-ful bg-base-100 shadow rounded-lg text-center flex justify-center items-center flex-col p-6'>
                     <h1 className='text-3xl text-[#244D3F] font-semibold'>{shouldBeFriends.days_since_contact}</h1>
                     <p className='text-[#64748B] text-lg'>Days Since Contact</p>
                 </div>
-                <div className='w-full h-ful bg-base-100 shadow rounded-lg text-center flex justify-center items-center flex-col'>
+                <div className='w-full h-ful bg-base-100 shadow rounded-lg text-center flex justify-center items-center flex-col p-6'>
                     <h1 className='text-3xl text-[#244D3F] font-semibold'>{shouldBeFriends.goal}</h1>
                     <p className='text-[#64748B] text-lg'>Goal (Days)</p>
                 </div>
-                <div className='w-full h-ful bg-base-100 shadow rounded-lg text-center flex justify-center items-center flex-col'>
+                <div className='w-full h-ful bg-base-100 shadow rounded-lg text-center flex justify-center items-center flex-col p-6'>
                     <h1 className='text-3xl text-[#244D3F] font-semibold'>{shouldBeFriends.next_due_date}</h1>
                     <p className='text-[#64748B] text-lg'>Next Due</p>
                 </div>
-                <div className='w-full h-ful bg-base-100 shadow rounded-lg col-span-3'></div>
-                <div className='w-full h-ful bg-base-100 shadow rounded-lg col-span-3'></div>
+                <div className='w-full h-ful bg-base-100 p-6 shadow rounded-lg col-span-3 flex justify-between items-center'>
+                    <div>
+                        <h1 className='font-medium text-[#244D3F] text-xl mb-6'>Relationship Goal</h1>
+                        <p className='font-medium text-[#64748B] text-lg'>Connects every
+                            <span className='text-[#1F2937] font-bold'> {shouldBeFriends.days_since_contact} days</span>
+                        </p>
+                    </div>
+                    <div>
+                        <button className='btn'>Edit</button>
+                    </div>
+                </div>
+                <div className='w-full h-ful bg-base-100 p-6 shadow rounded-lg col-span-3'>
+                    <h1 className='font-medium text-[#244D3F] mb-4 text-xl'>Quick Check-In</h1>
+                    <div className='grid grid-cols-3 gap-4'>
+                        <div className='h-24 flex items-center flex-col text-[#1F2937] text-xl justify-center btn btn-success btn-disabled rounded-lg'>
+                            <TbPhoneCall />
+                            <h1>Call</h1>
+                        </div>
+                        <div className='h-24 flex items-center flex-col text-[#1F2937] text-xl justify-center btn btn-success btn-disabled rounded-lg'>
+                            <MdOutlineSms />
+                            <h1>Text</h1>
+                        </div>
+                        <div className='h-24 flex items-center flex-col text-[#1F2937] text-xl justify-center btn btn-success btn-disabled rounded-lg'>
+                            <FiVideo />
+                            <h1>Video</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div >
     );
